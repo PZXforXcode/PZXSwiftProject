1a7beeb5c30a2135f54aaa4d40e9d2f78d753cbf
pzx <pengzuxinXcode@outlook.com>
2023-12-18 16:39:14 +0800

    新增添加阴影方法

--- PZXSwiftProjectSystemTabbar/PZXSwiftProject/Extension/UIView+Function.swift
+++ PZXSwiftProjectSystemTabbar/PZXSwiftProject/Extension/UIView+Function.swift
@@ -42,5 +42,14 @@ extension UIView {
         lineView.layer.addSublayer(shapeLayer)
     }
     
+    //添加阴影
+    func addShadow(color: UIColor = .black, opacity: Float = 0.5, offset: CGSize = CGSize(width: 0, height: 2), radius: CGFloat = 4) {
+            layer.shadowColor = color.cgColor
+            layer.shadowOpacity = opacity
+            layer.shadowOffset = offset
+            layer.shadowRadius = radius
+            layer.masksToBounds = false
+        }
+    
     
 }
