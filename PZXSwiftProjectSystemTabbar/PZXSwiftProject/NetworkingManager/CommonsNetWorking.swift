af6f70f2ab87db4e05126822dbb4aebaeed5f1eb
pzx <pengzuxinXcode@outlook.com>
2024-01-06 08:31:41 +0800

    新增requestDicFunc的方法用于网络请求出字典数据，用于简单的请求 不用建立model

---
+++ PZXSwiftProjectSystemTabbar/PZXSwiftProject/NetworkingManager/CommonsNetWorking.swift
@@ -0,0 +1,36 @@
+//
+//  CommonsNetWorking.swift
+//  iOS-PINGALAX-alps-app
+//
+//  Created by 彭祖鑫 on 2024/1/5.
+//  Copyright © 2024 PINGALAX. All rights reserved.
+//
+
+import Foundation
+
+
+func  manageAlias(deviceId:String){
+    
+//    var apiModel = APIModel(requestConfig: manageAliasAPI,paramDic: [
+//        "deviceId":"191e35f7e1448efbe9e"
+//    ])
+//
+//
+//    
+//    NetworkingManager.requestDicFunc(apiModel: &apiModel) { dic in
+//        
+//        let data = dic!["data"] as? NSDictionary
+//        let desc = data?["desc"]
+//        print("dic = \(String(describing: dic))")
+//        print("desc = \(String(describing: desc))")
+//
+//    } failure: { error in
+//        
+//        
+//        
+//    }
+
+
+    
+    
+}
--- PZXSwiftProjectSystemTabbar/PZXSwiftProject/NetworkingManager/NetworkingManager.swift
+++ PZXSwiftProjectSystemTabbar/PZXSwiftProject/NetworkingManager/NetworkingManager.swift
@@ -9,8 +9,12 @@ import Foundation
 import Alamofire
 
 typealias NetSuccessClosure<T: Codable> = (_ baseModel: NetBaseModel<T>?) -> Void
+typealias NetSuccessDic = (_ dic: [String: Any]?) -> Void
 typealias NetFailedClosure = (_ error: NetworkingError) -> Void
 typealias NetRequestClosure = (_ success: Bool, _ message: String ) -> Void
+
+let timeoutInterval: TimeInterval = 10 // 设置超时时间为10秒
+
 
 public func convertJSONToModel<T: Codable>(jsonObject: AnyObject, modelType: T.Type) -> T? {
      do {
@@ -29,7 +33,7 @@ class NetworkingManager {
 
         let header = NetworkingManager.settingDefaultHttpHeaders(apiModel: apiModel)
 
-        AF.request(apiModel.gettingFullUrl(), method: apiModel.requestConfig!.method, parameters: apiModel.paramDic, headers: header).response { (response) in
+        AF.request(apiModel.gettingFullUrl(), method: apiModel.requestConfig!.method, parameters: apiModel.paramDic, headers: header,requestModifier: {$0.timeoutInterval = timeoutInterval}).response { (response) in
 
             switch response.result {
             case .success(_):
@@ -80,7 +84,70 @@ class NetworkingManager {
             }
         }
     }
-
+    
+    public class func requestDicFunc(apiModel: inout APIModel, success: @escaping NetSuccessDic, failure: @escaping NetFailedClosure) {
+        
+        let header = NetworkingManager.settingDefaultHttpHeaders(apiModel: apiModel)
+        
+        AF.request(apiModel.gettingFullUrl(), method: apiModel.requestConfig!.method, parameters: apiModel.paramDic, headers: header,requestModifier: {$0.timeoutInterval = timeoutInterval}).response { (response) in
+            
+            switch response.result {
+            case .success(_):
+                let json = response.data ?? Data()
+                let jsonString = String(data: response.data ?? Data(), encoding: .utf8) ?? ""
+                // 打印请求的 URL
+                if let url = response.request?.url {
+                    print("Request URL: \(url)")
+                }
+                do {
+                    if let data = jsonString.data(using: .utf8) {
+                        if let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                            print("jsonObject = \(jsonObject)")
+                            success(jsonObject)
+                        }
+                    }
+                } catch {
+                    print("Error converting JSON to [String: Any]: \(error)")
+                    failure(NetworkingError(code: ErrorCode.Analysis.rawValue, localizedDescription:"网络错误"))
+                }
+            case let .failure(error):
+                if error.isResponseSerializationError {
+                    let json = response.data ?? Data()
+                    
+                    do {
+                        if let jsonObject = try JSONSerialization.jsonObject(with: json, options: []) as? [String: Any] {
+                            if let code = jsonObject["code"] as? String, code == "0" {
+                                // token失效
+                                NetworkingManager.TokenExpire()
+                            } else {
+                                // 解析错误
+                                let model = NetworkingError(code: IntSafe(jsonObject["code"] as? String ?? ErrorCode.Analysis.rawValue),
+                                                            localizedDescription: jsonObject["msg"] as? String ?? "")
+                                failure(model)
+                            }
+                        } else {
+                            // 解析错误
+                            let desError = NetworkingError(code: error.responseCode ?? ErrorCode.Analysis.rawValue,
+                                                           localizedDescription: error.localizedDescription)
+                            failure(desError)
+                        }
+                    } catch {
+                        // 解析错误
+                        let desError = NetworkingError(code: ErrorCode.Analysis.rawValue,
+                                                       localizedDescription: error.localizedDescription)
+                        failure(desError)
+                    }
+                    
+                } else {
+                    if let responseCode = error.responseCode {
+                        failure(NetworkingError.init(code: responseCode, localizedDescription: "网络错误"))
+                    } else {
+                        failure(NetworkingError.init(code: ErrorCode.Unkonwn.rawValue, localizedDescription:"网络错误"))
+                    }
+                }
+            }
+        }
+    }
 
     
     
